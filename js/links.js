a74aaeb3ed1d47673114f71e4cedf585633f7d55
rodrigoamorim <all.rodrigoamorim@gmail.com>
2026-01-03 23:43:49 -0300

    removi o icone dos links

--- js/links.js
+++ js/links.js
@@ -5,28 +5,28 @@ export const LINKS = [
     label: "Instagram",
     url: "https://instagram.com/eu.rodrigoamorim",
     category: "Redes",
-    icon: "📷",
+    icon: "",
   },
   {
     id: "youtube",
     label: "YouTube",
     url: "https://www.youtube.com/@eurodrigoamorim",
     category: "Conteúdo",
-    icon: "🎥",
+    icon: "",
   },
   {
     id: "github",
     label: "GitHub",
     url: "https://github.com/allrodrigoamorim",
     category: "Dev",
-    icon: "💻",
+    icon: "",
   },
   {
   id: "telegram",
   label: "Telegram",
   category: "Redes",
   url: "https://t.me/eurodrigoamorim?text=Olá%20Rodrigo!%20Vim%20pelo%20seu%20LinkHub%20🙂",
-  icon: "✈️",
+  icon: "",
 }
 
  ];
